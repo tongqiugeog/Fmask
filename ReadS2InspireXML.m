dfb490257a9fe332042f2fc6ad946d22548d9641
Shi Qiu <qasim99@163.com>
2020-03-17 21:38:03 -0400

    Update ReadS2InspireXML.m

--- ReadS2InspireXML.m
+++ ReadS2InspireXML.m
@@ -1,6 +1,7 @@
 function bbox = ReadS2InspireXML(xml_path)
 %READS2INSPIREXML Load INSPIRE XML data at .SAFE directory, and can give us
 %image extent.
+% The range of Sentinel2's longtitude is [0 360] sometimes, which need to be [-180 180]  by Shi 3/17/2020
     FileName = fullfile(xml_path,'INSPIRE.xml');
     S = xml2struct(FileName);
     clear FileName;
@@ -12,5 +13,12 @@ function bbox = ReadS2InspireXML(xml_path)
         str2double(extentS.gmd_colon_southBoundLatitude.gco_colon_Decimal.Text),...
         str2double(extentS.gmd_colon_westBoundLongitude.gco_colon_Decimal.Text),...
         str2double(extentS.gmd_colon_eastBoundLongitude.gco_colon_Decimal.Text)];
+    
+    if bbox(3) > 180
+        bbox(3) = bbox(3) - 360;
+    end
+    if bbox(4) > 180
+        bbox(4) = bbox(4) - 360;
+    end
 end
 
